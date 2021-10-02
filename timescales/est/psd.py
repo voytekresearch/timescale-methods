83ae54d243bae937433bfb82d1aa4c8f2287bafc
ryanhammonds <ryan.hammonds@utexas.edu>
2021-10-01 17:52:57 -0700

    add spectral tau estimation func

--- timescales/est/psd.py
+++ timescales/est/psd.py
@@ -3,6 +3,25 @@
 import numpy as np
 
 
+def fit_psd(spikes, fs, f_range, fooof_init=None, n_jobs=-1):
+
+    freqs, powers = compute_spectrum(spikes, fs, f_range=f_range)
+
+    if fooof_init is not None:
+        fm = FOOOF(aperiodic_mode='knee', verbose=False, **fooof_init)
+    else:
+        fm = FOOOF(aperiodic_mode='knee', verbose=False)
+
+    fm.fit(freqs, powers, f_range)
+
+    knee = fm.get_params('aperiodic', 'knee')
+    exp = fm.get_params('aperiodic', 'exponent')
+
+    knee_freq, knee_tau = convert_knee_val(knee, exponent=exp)
+
+    return fm, knee_freq, knee_tau
+
+
 def convert_knee_val(knee, exponent=2.):
     """Convert knee parameter(s) to frequency and time-constant value.
 
