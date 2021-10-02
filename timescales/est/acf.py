443149e2f8537f87cd7761c8dcf5a0e05a5e59bc
ryanhammonds <ryan.hammonds@utexas.edu>
2021-10-01 17:52:25 -0700

    add acf + cos fitting func

--- timescales/est/acf.py
+++ timescales/est/acf.py
@@ -7,6 +7,10 @@ import numpy as np
 from scipy.optimize import curve_fit
 from statsmodels.tsa.stattools import acf
 
+from neurodsp.filt import filter_signal
+from neurodsp.spectral import compute_spectrum
+from neurodsp.utils.data import create_times
+
 
 def fit_acf(spikes, nlags, guess=None, bounds=None, mode='mean', n_jobs=-1, maxfev=1000):
     """Compute and fit ACF.
@@ -95,6 +99,83 @@ def fit_acf(spikes, nlags, guess=None, bounds=None, mode='mean', n_jobs=-1, maxf
         return corrs_2d, params
     else:
         return corrs, params
+
+
+
+
+def fit_acf_cos(spikes, fs, nlags):
+    """
+    """
+    # Compute autcorr
+    ys = acf(spikes, nlags=nlags, qstat=False, fft=True)[1:]
+    xs = np.arange(len(ys))
+
+    corrs = acf(ys, nlags=nlags, qstat=False, fft=True)[1:]
+
+    # Compute spectrum of autocorrs to determine cos freq
+    f, p = compute_spectrum(ys, len(ys))
+    freq = int(np.argmax(p))
+
+    # Tau estimation
+    inds = np.where(np.diff(np.sign(np.diff(ys))) < 0)[0] + 1
+
+    exp_est = ys[inds].copy()
+    exp_est -= np.min(exp_est)
+    exp_est_interp = np.interp(np.arange(xs[inds][0], xs[inds][-1]+1), xs[inds], exp_est)
+    exp_est_bl = exp_est_interp - exp_est_interp[0] / np.exp(1)
+
+    _inds = np.where(exp_est_bl < 0)[0]
+
+    if len(_inds) == 0:
+        tau_guess = inds[0] / fs
+    else:
+        pts = [_inds[0]-2, _inds[0]-1]
+        tau_guess = (pts[np.argmin(exp_est_bl[pts])] + inds[0]) / fs
+
+    # Fit
+    p0 = [tau_guess, 1, 1, 1, .5]
+
+    bounds = [
+        (tau_guess * .1, .1, .1, .1, 0),
+        (tau_guess *  1, 10, 1,   1, 1)
+    ]
+
+
+    params, _ = curve_fit(lambda xs, t, g, ve, vc, vce, : sim_acf_cos(xs, fs, freq, t, g,
+                                                                      ve, vc, vce, True),
+                          xs, ys, p0=p0, bounds=bounds, maxfev=100)
+
+    return ys, freq, params
+
+
+def sim_acf_cos(xs, fs, cos_freq, tau, cos_gamma, var_exp, var_cos,
+                var_cos_exp, return_sum=True):
+    """
+    """
+    exp = np.exp(-np.arange(len(xs))/(tau * fs)) * var_exp
+    cos = sim_damped_oscillation(1, len(xs), cos_freq, cos_gamma, var_cos,
+                                    var_cos_exp)
+
+    if return_sum:
+        return exp+cos
+
+    return exp, cos
+
+
+def sim_damped_oscillation(n_seconds, fs, freq, gamma, var_cos, var_cos_exp):
+    """
+    """
+
+    times = create_times(n_seconds, fs)
+
+    exp = np.exp(-1 * gamma * times) * var_cos
+    cos = np.cos(2 * np.pi * freq * times) * var_cos_exp
+
+
+    return (exp * cos)
+
+
+
 
 
 def exp_decay_func(delta_t, amplitude, tau, offset):
