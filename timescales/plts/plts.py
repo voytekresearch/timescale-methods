1d33f34cb1c2b81614f5a083deb0e73f3bc20f2a
ryanhammonds <Ryan.hammonds@utexas.edu>
2022-03-16 20:12:39 -0700

    update connected scatter plt func

--- timescales/plts/plts.py
+++ timescales/plts/plts.py
@@ -3,14 +3,16 @@
 import numpy as np
 
 
-def plot_connected_scatter(param_a, param_b, ax, title, paired=True, alpha_scatter=.5,
-                           alpha_line=.1,  ylim=None, ylabel=None, xticklabels=None):
+def plot_connected_scatter(param_a, param_b, ax, title=None, paired=True,
+                           scatter_a_loc=None, scatter_b_loc=None, violin_locs=None,
+                           colors=None, alpha_scatter=.5, alpha_line=.1,
+                           xticks=None, xlim=None, ylim=None, ylabel=None, xticklabels=None):
     """Plot connected violin scattter plots.
     Parameters
     ----------
-    param_a : 1d array
+    param_a : 1d or 2d array
         First parameter to plot.
-    param_b : 1d array
+    param_b : 1d or 2d array
         Second parameter to plot.
     ax : AxesSubplot
         Subplot to plot onto.
@@ -29,17 +31,46 @@ def plot_connected_scatter(param_a, param_b, ax, title, paired=True, alpha_scatt
     xticklabels: list, optional, default: None
         X-axis labels.
     """
-    xs_a = np.random.uniform(1, 1.2, size=len(param_a))
+
+
+    if title is None:
+        title = ''
+
+    # Scatter points
+    if scatter_a_loc is None:
+        xs_a = np.random.uniform(1, 1.2, size=len(param_a))
+    else:
+        xs_a = np.random.uniform(*scatter_a_loc, size=len(param_a))
+
+    if scatter_a_loc is None:
+        xs_b = np.random.uniform(1.8, 2, size=len(param_b))
+    else:
+        xs_b = np.random.uniform(*scatter_b_loc, size=len(param_b))
+
+
     ax.scatter(xs_a, param_a, alpha=alpha_scatter)
-
-
-    xs_b = np.random.uniform(1.8, 2, size=len(param_b))
     ax.scatter(xs_b, param_b, alpha=alpha_scatter)
 
-    vp = ax.violinplot([param_a, param_b], showextrema=False)
-    vp['bodies'][1].set_color('C1')
-    vp['bodies'][1].set_facecolor('C1')
-    vp['bodies'][1].set_edgecolor('C1')
+    # Violinplots
+    if violin_locs is None:
+        vp = ax.violinplot([param_a, param_b], showextrema=False)
+    else:
+        vp = ax.violinplot([param_a, param_b], positions=violin_locs, showextrema=False)
+
+    if colors is None:
+        c0 = 'C0'
+        c1 = 'C1'
+    else:
+        c0 = colors[0]
+        c1 = colors[1]
+
+    vp['bodies'][0].set_color(c0)
+    vp['bodies'][0].set_facecolor(c0)
+    vp['bodies'][0].set_edgecolor(c0)
+
+    vp['bodies'][1].set_color(c1)
+    vp['bodies'][1].set_facecolor(c1)
+    vp['bodies'][1].set_edgecolor(c1)
 
     b = vp['bodies'][0]
     b.get_paths()[0].vertices[:, 0] = np.clip(b.get_paths()[0].vertices[:, 0], -np.inf,
@@ -53,20 +84,27 @@ def plot_connected_scatter(param_a, param_b, ax, title, paired=True, alpha_scatt
         for i, (t_psd, t_acf) in enumerate(zip(param_a, param_b)):
             ax.plot([xs_a[i], xs_b[i]], [t_psd, t_acf], color='k', alpha=alpha_line)
 
+
+
+    # Axis settings
     ax.set_title(title)
     ax.set_ylabel('Tau')
-    ax.set_xticks([1, 2])
+
+    if xticks is None:
+        ax.set_xticks([1, 2])
+    else:
+        ax.set_xticks(xticks)
 
     xticklabels = ['PSD', 'ACF'] if xticklabels is None else xticklabels
-    ax.set_xticklabels(xticklabels)
-    ax.set_xlim(0, 3)
+    ax.set_xticklabels(xticklabels, fontsize=24)
 
     if ylabel is None:
         ax.set_ylabel('Tau')
     else:
         ax.set_ylabel(ylabel)
 
+    if xlim is not None:
+        ax.set_xlim(xlim)
+
     if ylim is not None:
         ax.set_ylim(ylim)
-
-    return ax
