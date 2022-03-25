287fcc3f8c135e0396ed2edfc603d60eaf9b8f85
ryanhammonds <Ryan.hammonds@utexas.edu>
2022-03-24 20:37:55 -0700

    add AR decomposition module

--- timescales/decompose/__init__.py
+++ timescales/decompose/__init__.py
@@ -1,3 +1,5 @@
 """Initialize autoregressive PSD estimation."""
 
 from .ar_spectrum import ar_psd, ar_psds_bandstop
+from .decompose import (decompose_ar, decompose_ar_windows, gen_ar_fit,
+    sim_asine, iter_estimate_freq, estimate_freq)
