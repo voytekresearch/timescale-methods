2f09fa3eec745edd79205580c6d025d555be7215
ryanhammonds <Ryan.hammonds@utexas.edu>
2022-04-29 18:50:54 -0700

    update fit tests

--- timescales/tests/fit/test_fit_acf.py
+++ timescales/tests/fit/test_fit_acf.py
@@ -3,9 +3,9 @@
 import pytest
 import numpy as np
 
-from timescales.fit import ACF, compute_acf, fit_acf, fit_acf_cos
+from timescales.fit import ACF, fit_acf, fit_acf_cos
 from timescales.fit.acf import _acf_proxy, _acf_cos_proxy
-from timescales.sim import sim_spikes_synaptic, sim_acf_cos, sim_exp_decay
+from timescales.sim import sim_acf_cos, sim_exp_decay
 
 
 
@@ -13,7 +13,7 @@ def test_ACF_init():
     corrs = np.random.rand(100)
     lags = np.arange(1, 101)
     fs = 100
-    acf = ACF(corrs, lags, fs)
+    acf = ACF(lags, corrs, fs)
 
     assert (acf.corrs == corrs).all()
     assert (acf.lags == lags).all()
@@ -24,110 +24,93 @@ def test_ACF_compute_acf(from_psd):
 
     sig = np.random.rand(1000)
     fs = 100
-    start = 100
-    win_len = 100
 
     acf = ACF()
-    acf.compute_acf(sig, fs, start, win_len, from_psd=from_psd)
-
+    acf.compute_acf(sig, fs, from_psd=from_psd)
     assert acf.corrs is not None
     assert abs(np.mean(acf.corrs)) < .25
 
+    acf = ACF()
+    acf.compute_acf(np.vstack((sig, sig)), fs, from_psd=from_psd, n_jobs=2)
+    print(np.vstack((sig, sig)).shape)
+    print(acf.corrs.shape)
+    assert (acf.corrs[0] == acf.corrs[1]).all()
 
-@pytest.mark.parametrize('low_mem', [False])
-def test_ACF_fit(low_mem):
+    # Expect error
+    if from_psd is False:
+        sig = np.random.rand(2, 2, 1000)
+        acf = ACF()
+
+        with pytest.raises(ValueError):
+            acf.compute_acf(sig, fs, from_psd=from_psd)
+
+
+@pytest.mark.parametrize('with_cos', [True, False])
+@pytest.mark.parametrize('gen_fits', [True, False])
+@pytest.mark.parametrize('ndim', [1, 2])
+def test_ACF_fit(with_cos, gen_fits, ndim):
     lags = np.arange(1, 101)
     tau = .01
     amp = 1
     fs = 1000
     corrs = sim_exp_decay(lags, fs, tau, amp)
-    acf = ACF(corrs, lags, fs, low_mem)
-    acf.fit()
-    acf.gen_corrs_fit()
-
-    assert (acf.params[0] - tau) < (tau * .1)
-    assert acf.rsq > .5
-
-@pytest.mark.parametrize('low_mem', [True, False])
-def test_ACF_fit_cos(low_mem):
-
-    xs = np.arange(1, 100)
-    fs = 1000
-    freq = 10
-    tau = .01
-    osc_tau = .25
-    osc_gamma = .01
-    amp_ratio = .8
-    height = 1
-    offset = 0
-
-    corrs = sim_acf_cos(xs, fs, tau, osc_tau, osc_gamma, freq, amp_ratio, height, offset)
-
-    acf = ACF(corrs, xs, fs, low_mem)
-    acf.fit_cos()
-    acf.gen_corrs_fit()
-
-    assert (acf.params[0] - tau) < (tau * .1)
-    assert acf.rsq > .5
-
-
-@pytest.mark.parametrize('ndim_mode',
-    [(1, None), (2, None), (2, 'mean'), (2, 'median'), (2, 'invalid'), (3, None)]
-)
-def test_compute_acf(ndim_mode):
-
-    ndim, mode = ndim_mode
-    n_seconds = 1
-    fs = 1000
-    tau = .01
-    nlags = 10
-    n_neurons = 2
-
-    probs, _ = sim_spikes_synaptic(n_seconds, fs, tau, n_neurons=n_neurons, return_sum=True)
 
     if ndim == 2:
-        probs = np.tile(probs, (2, 1))
-    if ndim == 3:
-        probs = np.tile(probs, (3, 2, 1))
+        corrs = np.vstack((corrs, corrs))
 
-    if mode == 'invalid' or ndim == 3:
+    acf = ACF(lags, corrs, fs)
+    acf.fit(gen_fits=gen_fits)
+
+    if ndim == 1:
+        assert (acf.params[0] - tau) < (tau * .1)
+    else:
+        assert (acf.params[0, 0] - tau) < (tau * .1)
+
+    if gen_fits:
+        assert acf.corrs_fit is not None
+
+        if ndim == 1:
+            assert acf.rsq > .5
+        else:
+            assert acf.rsq.mean() > .5
+
+    # Errors
+    acf = ACF()
+    with pytest.raises(ValueError):
+        acf.fit()
+
+
+@pytest.mark.parametrize('with_cos', [True, False])
+@pytest.mark.parametrize('gen_components', [True, False])
+@pytest.mark.parametrize('ndim', [1, 2])
+def test_ACF_gen_corrs_fit(with_cos, gen_components, ndim):
+
+    lags = np.arange(1, 101)
+    tau = .01
+    amp = 1
+    fs = 1000
+    corrs = sim_exp_decay(lags, fs, tau, amp)
+
+    if ndim == 2:
+        corrs = np.vstack((corrs, corrs))
+
+    acf = ACF(lags, corrs, fs)
+    # Note:
+    # Can't sparate components when there is only one
+    if gen_components and not with_cos:
         with pytest.raises(ValueError):
-            corrs = compute_acf(probs, nlags, mode=mode, n_jobs=-1, progress=None)
-        return
+            acf.fit(with_cos=with_cos, gen_fits=True, gen_components= gen_components)
     else:
-        corrs = compute_acf(probs, nlags, mode=mode, n_jobs=-1, progress=None)
-
-
-    if ndim == 2 and mode is None:
-        assert corrs.ndim == 2
-        corrs = corrs.mean(axis=0)
-    else:
-        assert corrs.ndim == 1
-
-    assert len(corrs) == nlags
-    assert np.max(corrs <= 1)
-    assert np.min(corrs >= -1)
+        acf.fit(with_cos=with_cos, gen_fits=True, gen_components= gen_components)
 
 
 @pytest.mark.parametrize('ndim', [1, 2])
 @pytest.mark.parametrize('bounds', [True, None])
 def test_fit_acf(ndim, bounds):
 
-    n_seconds = 1
     fs = 1000
     tau = .01
-    nlags = 10
-    n_neurons = 2
-
-    if bounds is True:
-        bounds = [
-            (.01, 0, 0, 0, 0, 0, -.5),
-            (1  ,  1, 1, 1, .1, 10, .5)
-        ]
-
-    # Fit spikes
-    probs, _ = sim_spikes_synaptic(n_seconds, fs, tau, n_neurons=n_neurons, return_sum=True)
-    corrs = compute_acf(probs, nlags, n_jobs=-1, progress=None)
+    corrs = sim_exp_decay(np.arange(1, 100), fs, tau, 1)
 
     if ndim == 2:
         corrs = np.tile(corrs, (2, 1))
--- timescales/tests/fit/test_psd.py
+++ timescales/tests/fit/test_psd.py
@@ -5,15 +5,61 @@ import pytest
 import numpy as np
 
 from fooof import FOOOF, FOOOFGroup
+from fooof.core.funcs import expo_const_function
 from neurodsp.spectral import compute_spectrum
 
-from timescales.fit.psd import fit_psd, convert_knee_val
+from timescales.fit.psd import PSD, fit_psd_fooof, fit_psd_huber, convert_knee_val
 from timescales.sim import sim_spikes_synaptic
 
 
+def test_psd_init():
+    freqs = [0, 1]
+    powers = [2, 3]
+
+    psd = PSD(freqs, powers)
+    assert (psd.freqs == freqs)
+    assert (psd.powers == powers)
+
+@pytest.mark.parametrize('ar_order', [None, 1])
+def test_psd_compute_spectrum(ar_order):
+
+    n_seconds = 1
+    fs = 1000
+    tau = .01
+
+    sig, _ = sim_spikes_synaptic(n_seconds, fs, tau, n_neurons=1, return_sum=True)
+
+    psd = PSD()
+
+    psd.compute_spectrum(sig, fs, ar_order=ar_order)
+
+    assert isinstance(psd.freqs, np.ndarray)
+    assert isinstance(psd.powers, np.ndarray)
+
+@pytest.mark.parametrize('ndim', [1, 2])
+@pytest.mark.parametrize('f_range', [None, (1, 100)])
+@pytest.mark.parametrize('method', ['huber', 'fooof', 'invalid'])
+def test_psd_fit(ndim, f_range, method):
+
+    freqs = np.arange(0, 101)
+    powers = 10**expo_const_function(freqs, 0, 10, 2, .001)
+
+    if ndim == 2:
+        powers = np.vstack((powers, powers))
+
+    psd = PSD(freqs, powers)
+
+    if method == 'invalid':
+        with pytest.raises(ValueError):
+            psd.fit(f_range=f_range, method=method)
+    else:
+        psd.fit(f_range=f_range, method=method)
+        assert psd.powers_fit.shape == psd.powers.shape
+
+@pytest.mark.parametrize('ap_mode', ['knee', 'knee_constant'])
 @pytest.mark.parametrize('ndim', [1, 2])
 @pytest.mark.parametrize('init', [True, False])
-def test_fit_psd(ndim, init):
+def test_fit_psd_fooof(ap_mode, ndim, init):
 
     n_seconds = 1
     fs = 1000
@@ -25,24 +71,22 @@ def test_fit_psd(ndim, init):
     if ndim == 2:
         probs = np.tile(probs, (2, 1))
 
-    freqs, powers = compute_spectrum(probs, fs)
+    freqs, powers = compute_spectrum(probs, fs, f_range=(1, 100))
 
     if init:
-        fooof_init = {'max_n_peaks': 0}
-        fm, knee_freq = fit_psd(freqs, powers, (1, 100), fooof_init=fooof_init)
+        fooof_init = {'max_n_peaks': 0, 'aperiodic_mode': ap_mode}
+        params, powers_fit = fit_psd_fooof(freqs, powers, (1, 100), fooof_init=fooof_init)
     else:
-        fm, knee_freq = fit_psd(freqs, powers, (1, 100))
+        params, powers_fit = fit_psd_fooof(freqs, powers, (1, 100))
 
-    if np.isnan(knee_freq).any():
-        assert isinstance(fm, (FOOOF, FOOOFGroup))
-        return
-
-    if ndim == 1:
-        assert isinstance(fm, FOOOF)
-        assert isinstance(knee_freq, float)
-    elif ndim == 2:
-        assert isinstance(fm, FOOOFGroup)
-        assert (knee_freq[0] == knee_freq[1]).all()
+    if ap_mode == 'knee_constant' and ndim == 1:
+        assert len(powers_fit) == len(powers)
+        assert len(params) == 4
+    elif ap_mode == 'knee_constant' and ndim == 2:
+        assert len(powers_fit[0]) == len(powers[0])
+        assert len(powers_fit) == len(powers)
+        assert len(params) == 2
+        assert len(params[0]) == 4
 
 
 def test_convert_knee_val():
