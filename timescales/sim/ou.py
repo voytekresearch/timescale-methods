46fea5f406a060de7a7b43f8a8c03024f6080fdc
ryanhammonds <Ryan.hammonds@utexas.edu>
2022-05-12 19:27:09 -0700

    api update

--- timescales/sim/ou.py
+++ timescales/sim/ou.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def sim_spikes_ou(n_seconds, fs, tau, n_neurons=100, std=.1, mu=10., return_sum=True):
+def sim_ou(n_seconds, fs, tau, std=.1, mu=10.):
     """Simulate spikes as an Ornstein-Uhlenbeck process.
 
     Parameters
@@ -14,19 +14,15 @@ def sim_spikes_ou(n_seconds, fs, tau, n_neurons=100, std=.1, mu=10., return_sum=
         Sampling rate, in hz.
     tau : float
         Timescale, in seconds.
-    n_neurons : int, optional, default: 100
-        Number of neurons to simulate.
     std : float, optional, default: 1
         Standard deviation of the OU process.
     mu : float, optional, default: None
         Mean of of the OU process.
-    return_sum : bool, optional, default: True
-        Returns sum of neurons if True. If False, a 2d binary array is returned.
 
     Returns
     -------
-    spikes : 1d or 2d array
-        Sum of spike counts across neurons.
+    sig : 1d or 2d array
+        Signal containing timescale of interest.
     """
 
     n_samples = int(n_seconds * fs)
@@ -35,22 +31,11 @@ def sim_spikes_ou(n_seconds, fs, tau, n_neurons=100, std=.1, mu=10., return_sum=
     sigma = std * np.sqrt(2. / tau)
     sqrtdt = np.sqrt(1/fs)
 
-    probs = np.zeros(n_samples)
+    sig = np.zeros(n_samples)
 
-    # Simulate the OU process and convert to probabilities
+    # Simulate the OU process
     for ind in range(n_samples - 1):
-        probs[ind + 1] = probs[ind] + (1/fs) * \
-            (-(probs[ind] - mu) / tau) + sigma * sqrtdt * np.random.randn()
+        sig[ind + 1] = sig[ind] + (1/fs) * \
+            (-(sig[ind] - mu) / tau) + sigma * sqrtdt * np.random.randn()
 
-    probs = (probs - np.min(probs)) / np.ptp(probs)
-
-    # Select [0=no spike, 1=spike] using probabilities
-    spikes = np.zeros((n_neurons, len(probs)), dtype=bool)
-
-    for ind in range(n_neurons):
-        spikes[ind] = (probs > np.random.rand(*probs.shape))
-
-    if return_sum:
-        spikes = spikes.sum(axis=0)
-
-    return spikes
+    return sig
--- timescales/tests/sim/test_ou.py
+++ timescales/tests/sim/test_ou.py
@@ -2,23 +2,17 @@
 
 import pytest
 
-from timescales.sim.ou import sim_spikes_ou
+from timescales.sim.ou import sim_ou
 
 
 @pytest.mark.parametrize('return_sum', [True, False])
-def test_sim_spikes_ou(return_sum):
+def test_sim_ou(return_sum):
 
     n_seconds = 1
     fs = 1000
     tau = .1
     n_neurons = 10
 
-    spikes = sim_spikes_ou(n_seconds, fs, tau, n_neurons=n_neurons, return_sum=return_sum)
+    spikes = sim_ou(n_seconds, fs, tau)
 
-    if return_sum:
-        assert spikes.dtype == int
-        assert len(spikes) == int(n_seconds * fs)
-    else:
-        assert spikes.dtype == bool
-        assert len(spikes) == n_neurons
-        assert len(spikes[0]) == int(n_seconds * fs)
+    assert len(spikes) == int(n_seconds * fs)
